dbe0a7bd814112113f73711976f1ad0ed01b9ef1
YueXiaoD <111783148+YueXiaoD@users.noreply.github.com>
2023-05-06 12:08:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -230,5 +230,5 @@ if __name__ == "__main__":
     # 公众号推送消息
     for user in users:
         send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en, max_temp, min_temp, sunrise,
-                     sunset, category, proposal, chp, birthday_data)
+                     sunset, category, proposal, chp, birthday)
     os.system("pause")
